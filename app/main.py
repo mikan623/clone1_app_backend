49d8edd824391e9a131068085e07cc8d6a559867
原　紫乃花 <shinoka@suzukinoMacBook-Pro.local>
2026-05-21 13:21:09 +0900

    FastAPIの連携ファイル移行

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@
 from fastapi import FastAPI
 # FastAPIの中の middleware フォルダの cors 機能から CORSMiddleware クラスを読み込む
 from fastapi.middleware.cors import CORSMiddleware
-# api フォルダのhello.py から router を読み込む
-from app.api import hello
+
+# APIRouterを使って、APIのルーティングを定義
+from fastapi import APIRouter
+router = APIRouter(prefix="/api/hello", tags=["hello"])
 
 # アプリ本体をを作成。appがWebアプリそのもの
 app = FastAPI()
@@ -19,7 +21,7 @@ app.add_middleware(
 
 @app.get("/api/hello")
 def read_root():
-    return {"message": "できた！"}
+    return {"message": "hello"}
 
 # router登録
-app.include_router(hello.router)
+app.include_router(router)
